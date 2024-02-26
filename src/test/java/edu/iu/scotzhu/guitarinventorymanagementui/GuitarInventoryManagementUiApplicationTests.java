8c397c075ce3720386544bccec38f6820ab1f1d9
Scott Zhu <szhu@stu.naperville203.org>
2024-02-25 20:53:02 -0500

    change

--- src/test/java/edu/iu/scotzhu/guitarinventorymanagementui/GuitarInventoryManagementUiApplicationTests.java
+++ src/test/java/edu/iu/scotzhu/guitarinventorymanagementui/GuitarInventoryManagementUiApplicationTests.java
@@ -2,7 +2,7 @@ package edu.iu.scotzhu.guitarinventorymanagementui;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test
 @SpringBootTest
 class GuitarInventoryManagementUiApplicationTests {
 
